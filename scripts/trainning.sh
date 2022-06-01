98e22a4856981873d137bd34d20b876623f27a1a
x0chaoren0s <x0chaoren0s@gmail.com>
2022-06-01 23:18:01 +0800

    lab 跑通 my_configs/deeplabv3plus_r50-d8_512x512_40k_voc12aug.py

--- scripts/trainning.sh
+++ scripts/trainning.sh
@@ -4,4 +4,4 @@
 #     configs/deeplabv3plus/deeplabv3plus_r50-d8_512x512_40k_voc12aug.py
 
 python train_copie_from_tools.py \
-    work_dirs/deeplabv3plus_r50-d8_512x512_40k_voc12aug/deeplabv3plus_r50-d8_512x512_40k_voc12aug.py
+    configs/my_configs/deeplabv3plus_r50-d8_512x512_40k_voc12aug.py
